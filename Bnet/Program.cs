c2ca831b33dc99746e6119997d0673b8529f8b94
Сергей <woronowserg@gmail.com>
2018-01-30 23:18:11 +0200

    Vol. 2

--- Bnet/Program.cs
+++ Bnet/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using Microsoft.Win32;
 
 namespace Bnet
 {
@@ -12,8 +13,8 @@ namespace Bnet
     {
 
         static IWebDriver Chrome;
-        static IWebElement element;
         static ChromeOptions opt;
+        static string userName;
 
         static void Main(string[] args)
         {
@@ -23,18 +24,22 @@ namespace Bnet
 
         public static void setUp()
         {
+            userName = System.Security.Principal.WindowsIdentity.GetCurrent().Name;
+            string output = userName.Substring(userName.IndexOf(@"\") + 1);
+            Console.WriteLine(output);
             opt = new ChromeOptions();
-            opt.AddArgument(@"user-data-directory=C:\Users\Сергей\AppData\Local\Google\Chrome\User Data\Default");
-            Chrome = new ChromeDriver(@"D:\", opt);
+            opt.AddArgument("--user-data-dir=C:/Users/" + output + "/AppData/Local/Google/Chrome/User Data/Default");
+            opt.AddArguments("disable-infobars");
+            Chrome = new ChromeDriver(@"C:\Program Files (x86)\Google\Chrome\Application\", opt);
         }
 
         public static void Execute()
         {
-            /*
+            
             Chrome.Navigate().GoToUrl(@"http://bnet2.000webhostapp.com/send.html");
             Chrome.FindElement(By.Name("btc_address")).SendKeys("test_btc_address");
             Chrome.FindElement(By.Name("btc_amount")).SendKeys("$$$");
-            */
+            
         }
 
         public static void Quit()
